db5e464b10dd1810b2c387822511afd510621abd
Mchtsz <mhatland206@gmail.com>
2024-04-05 13:35:23 +0200

    it works

--- app.ts
+++ app.ts
@@ -16,7 +16,7 @@ app.post('/createPostAdr', async (req, res) => {
                 Poststed: Poststed
             }
         });
-        res.send(post);
+        res.json(post);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'An error occurred while creating the post' });
@@ -26,7 +26,7 @@ app.post('/createPostAdr', async (req, res) => {
 app.post('/createAuthor', async (req, res) => {
     const { fornavn, etternavn, mail, passord, postID } = req.body;
     try {
-        const jornalist = await prisma.jornalist.create({
+        const journalist = await prisma.journalist.create({
             data: {
                 fornavn: fornavn,
                 etternavn: etternavn,
@@ -39,12 +39,158 @@ app.post('/createAuthor', async (req, res) => {
                 }
             }
         });
-        res.send(jornalist);
+        res.json(journalist);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'An error occurred while creating the post' });
     }
 });
+
+app.post('/createArticle', async (req, res) => {
+    const { title, content, views, updateAt, journalist } = req.body;
+
+    const j = await prisma.journalist.findFirst({
+        where: {
+            fornavn: {
+                startsWith: journalist
+            }
+        }
+    })
+    if (!j) {
+        return
+    }
+    const artikkel = await prisma.artikkel.create({
+        data: {
+            title: title,
+            content: content,
+            views: parseInt(views),
+            updateAt: new Date(updateAt),
+            journalister: {
+                connect: {
+                    id: j.id
+                }
+            }
+        }
+    }).catch((error) => {
+        console.error(error);
+        return 500;
+    });
+
+    if(artikkel === 500) {
+        return res.status(500).json({ error: 'An error occurred while fetching the article' });
+    }
+
+    if(!artikkel) {
+        return res.status(404).json({ error: 'Article not found' });
+    }
+
+    res.json(artikkel);
+});
+
+app.post("/updateArticle", async (req, res) => {
+    const { id, title, content, views, updateAt, journalist } = req.body;
+
+    const j = await prisma.journalist.findFirst({
+        where: {
+            fornavn: {
+                startsWith: journalist
+            }
+        }
+    });
+
+    if (!j) {
+        return
+    }
+
+    const artikkel = await prisma.artikkel.update({
+        where: {
+            id: parseInt(id)
+        },
+        data: {
+            title: title,
+            content: content,
+            views: parseInt(views),
+            updateAt: new Date(updateAt),
+            journalister: {
+                connect: {
+                    id: j.id
+                }
+            }
+        }
+    }).catch((error) => {
+        console.error(error);
+        return 500;
+    });
+
+    if(artikkel === 500) {
+        return res.status(500).json({ error: 'An error occurred while fetching the article' });
+    }
+
+    if(!artikkel) {
+        return res.status(404).json({ error: 'Article not found' });
+    }
+
+    res.json(artikkel);
+});
+
+app.get('/api/artikkel/:id', async (req, res) => {
+    const { id } = req.params;
+    const apiArtikkel = await prisma.artikkel.findUnique({
+        where: {
+            id: parseInt(id)
+        }
+    }).catch(() => 500)
+
+    if(apiArtikkel === 500) {
+        return res.status(500).json({ error: 'An error occurred while fetching the article' });
+    }
+
+    if(!apiArtikkel) {
+        return res.status(404).json({ error: 'Article not found' });
+    }
+
+    res.json(apiArtikkel);
+});
+
+app.get('/api/journalists/popular', async (req, res) => {
+    const popularPeople = await prisma.artikkel.findMany({
+        orderBy: { // sorterer artiklene etter views
+            views: 'desc' // descending
+        },
+
+        where: { // filtrerer hvilke artikler den henter
+            AND: [
+                {
+                    journalister: {
+                        some: {
+                            fornavn: {
+                                startsWith: "Jo"
+                            }
+                        }
+                    }
+                },
+
+                {
+                    journalister: {
+                        some: {
+                            fornavn: {
+                                startsWith: "Trond"
+                            }
+                        }
+                    
+                    }
+                }
+            ]
+        }
+    }).catch(() => 500);
+
+    if(popularPeople === 500) {
+        return res.status(500).json({ error: 'An error occurred while fetching the popular journalists' }); // hvis det ikke funker så console.logger den error
+    }
+
+    res.json(popularPeople); // sender data json
+});
+
 
 app.listen(3000, () => {
     console.log(`Server running on port 3000`);
